c2ac19d6ef69eb3ca0120467c6bd1b08349c8464
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2025-02-06 01:01:30 +0100

    refactored version

--- 2_simple_finance_agent.py
+++ 2_simple_finance_agent.py
@@ -26,15 +26,15 @@ def get_company_symbol(company: str) -> str:
 
 # Create an instance of the Agent
 agent_instance = Agent(
-    model=Groq(id="deepseek-r1-distill-llama-70b"),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True), get_company_symbol],
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
     show_tool_calls=True,
     markdown=True,
-    instructions=["Use tables to display data.", "If the company symbol is not known, please use the get_company_symbol tool"],
+    instructions=["Use tables to display data."],
     debug_mode=True
 
 )
 
 # Call the print_response method on the instance
-agent_instance.print_response("Summerize and compare analyst recommendations and fundamentals for TSLA and Phidata")
+agent_instance.print_response("Summerize and compare analyst recommendations and fundamentals for TSLA and AAPL.")
 
